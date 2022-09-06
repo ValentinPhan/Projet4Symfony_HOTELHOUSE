a6654e0333aa1cb65dd04cd7952f6673f945c3e9
Valentin <valentinnguyenphan@gmail.com>
2022-09-06 19:32:21 +0200

    Initial commit

---
+++ src/Entity/Chambre.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChambreRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChambreRepository::class)]
+class Chambre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description_courte = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description_longue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $photo = null;
+
+    #[ORM\Column]
+    private ?int $prix_journalier = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date_enregistrement = null;
+
+    #[ORM\OneToOne(mappedBy: 'chambre', cascade: ['persist', 'remove'])]
+    private ?Commande $commande = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescriptionCourte(): ?string
+    {
+        return $this->description_courte;
+    }
+
+    public function setDescriptionCourte(string $description_courte): self
+    {
+        $this->description_courte = $description_courte;
+
+        return $this;
+    }
+
+    public function getDescriptionLongue(): ?string
+    {
+        return $this->description_longue;
+    }
+
+    public function setDescriptionLongue(string $description_longue): self
+    {
+        $this->description_longue = $description_longue;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getPrixJournalier(): ?int
+    {
+        return $this->prix_journalier;
+    }
+
+    public function setPrixJournalier(int $prix_journalier): self
+    {
+        $this->prix_journalier = $prix_journalier;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $date_enregistrement): self
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+
+    public function getCommande(): ?Commande
+    {
+        return $this->commande;
+    }
+
+    public function setCommande(Commande $commande): self
+    {
+        // set the owning side of the relation if necessary
+        if ($commande->getChambre() !== $this) {
+            $commande->setChambre($this);
+        }
+
+        $this->commande = $commande;
+
+        return $this;
+    }
+}
